23d57f4380a29a905e8664299eabe7666cf5e224
AlbertoDelgado2 <albertitomdz013b@gmail.com>
2024-05-07 12:13:03 +0200

    Faltan Colores

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import WebAlberto from "./components/WebAlberto.vue"
 import EquipoTrabajo from "./components/EquipoTrabajo.vue"
+import InfoPag from "./components/InfoPag.vue"
 </script>
 
 <template>
 <WebAlberto />
 <EquipoTrabajo />
+<InfoPag />
 </template>
 
